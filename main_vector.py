b88b9b07267f80584b4571e7812c85971847db1c
wang xukun <wangxukun@hotmail.com>
2024-02-05 17:36:07 +0800

    命令行参数，脚本输入输出重定向

--- main_vector.py
+++ main_vector.py
@@ -1,8 +1,8 @@
+import sys
 from playLA.Vector import Vector
 import playLA.module1
 
 if __name__ == '__main__':
-
     vec = Vector([5, 2])
     print(vec)
     print(len(vec))
@@ -18,9 +18,18 @@ if __name__ == '__main__':
     print("+{} = {}".format(vec, +vec))
     print("-{} = {}".format(vec, -vec))
 
-    print("Printer print : ")
-    playLA.module1.printer(2025)
-
     zero2 = Vector.zero(2)
     print(zero2)
     print("{} + {} = {}".format(vec, zero2, vec + zero2))
+
+    print("Printer print : ")
+    playLA.module1.printer(2025)
+
+
+def demo():
+    contents = sys.stdin.read()
+    sys.stdout.write(contents.replace(sys.argv[1], sys.argv[2]))
+
+
+"""运行程序：python main_vector.py zero @ <./001.txt> outfile.txt"""
+demo()
